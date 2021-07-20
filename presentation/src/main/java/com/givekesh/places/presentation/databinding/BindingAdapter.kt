20752632c185b1b63b906b949f2c610ee57de56a
jinxul <ahmadgivekesh@gmail.com>
2021-07-20 22:12:30 +0430

    added glide module

--- presentation/src/main/java/com/givekesh/places/presentation/databinding/BindingAdapter.kt
+++ presentation/src/main/java/com/givekesh/places/presentation/databinding/BindingAdapter.kt
@@ -1,7 +1,12 @@
 package com.givekesh.places.presentation.databinding
 
+import com.bumptech.glide.RequestManager
+import com.givekesh.places.presentation.di.scope.BindingAdapterScope
 import javax.inject.Inject
 
-class BindingAdapter @Inject constructor() {
+@BindingAdapterScope
+class BindingAdapter @Inject constructor(
+    private val glide: RequestManager
+) {
 
 }
---
+++ presentation/src/main/java/com/givekesh/places/presentation/di/modules/GlideModule.kt
@@ -0,0 +1,43 @@
+package com.givekesh.places.presentation.di.modules
+
+import android.content.Context
+import com.bumptech.glide.Glide
+import com.bumptech.glide.RequestManager
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+import com.givekesh.places.presentation.di.component.BindingAdapterComponent
+import com.givekesh.places.presentation.di.scope.BindingAdapterScope
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+
+@Module
+@InstallIn(BindingAdapterComponent::class)
+object GlideModule {
+    @BindingAdapterScope
+    @Provides
+    fun provideDiskCacheStrategy(): DiskCacheStrategy = DiskCacheStrategy.AUTOMATIC
+
+    @BindingAdapterScope
+    @Provides
+    fun provideRequestOptionsFactory(
+        strategy: DiskCacheStrategy
+    ): Glide.RequestOptionsFactory = Glide.RequestOptionsFactory {
+        RequestOptions().diskCacheStrategy(strategy)
+    }
+
+    @BindingAdapterScope
+    @Provides
+    fun provideRequestOptions(
+        factory: Glide.RequestOptionsFactory
+    ): RequestOptions = factory.build()
+
+    @BindingAdapterScope
+    @Provides
+    fun provideGlide(
+        @ApplicationContext context: Context,
+        requestOptions: RequestOptions
+    ): RequestManager = Glide.with(context)
+        .applyDefaultRequestOptions(requestOptions)
+}
