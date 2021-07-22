2f62971057d658c53b6825b665d9e6df9cbf4c0f
jinxul <ahmadgivekesh@gmail.com>
2021-07-23 02:00:14 +0430

    fixed cached items sorting

--- data/src/main/java/com/givekesh/places/data/source/local/PlacesDao.kt
+++ data/src/main/java/com/givekesh/places/data/source/local/PlacesDao.kt
@@ -8,10 +8,14 @@ interface PlacesDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(data: List<CachedPlace>)
 
-    @Query("SELECT * FROM Places WHERE title LIKE '%' || :searchQuery || '%'")
+    @Query("SELECT * FROM Places WHERE title LIKE '%' || :searchQuery || '%' ORDER BY isPromoted DESC")
     suspend fun getPlaces(searchQuery: String): List<CachedPlace>
 
-    @Query("SELECT * FROM Places WHERE isFavorite = 1 AND title LIKE '%' || :searchQuery || '%'")
+    @Query(
+        "SELECT * FROM Places " +
+                "WHERE isFavorite = 1 AND title LIKE '%' || :searchQuery || '%' " +
+                "ORDER BY isPromoted DESC"
+    )
     suspend fun getFavorites(searchQuery: String): List<CachedPlace>
 
     @Query("UPDATE Places SET isFavorite = :favorite WHERE id = :id")
