5731883a13f58657dc0803a8dcb63edb7080da6f
jinxul <ahmadgivekesh@gmail.com>
2021-07-23 01:40:45 +0430

    added ratingbar to fragment_details.xml

--- presentation/src/main/java/com/givekesh/places/presentation/databinding/BindingAdapter.kt
+++ presentation/src/main/java/com/givekesh/places/presentation/databinding/BindingAdapter.kt
@@ -1,9 +1,13 @@
 package com.givekesh.places.presentation.databinding
 
+import android.content.res.ColorStateList
 import android.widget.ImageView
+import android.widget.RatingBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.RequestManager
+import com.givekesh.places.R
 import com.givekesh.places.presentation.di.scope.BindingAdapterScope
 import javax.inject.Inject
 
@@ -19,5 +23,19 @@ class BindingAdapter @Inject constructor(
     @BindingAdapter("setUnicodeText")
     fun setText(view: TextView, text: String) {
         view.text = text.replace("\\u200C", "\u200C")
+    }
+
+    @BindingAdapter("setRating")
+    fun setRating(view: RatingBar, score: Double) {
+        view.rating = score.toFloat()
+        val colorRes = when {
+            score > 4 -> R.color.ratingGood
+            score > 3 -> R.color.ratingNormal
+            score > 2 -> R.color.ratingFair
+            score > 1 -> R.color.ratingPoor
+            else -> R.color.ratingVeryPoor
+        }
+        val color = ContextCompat.getColor(view.context, colorRes)
+        view.progressTintList = ColorStateList.valueOf(color)
     }
 }
