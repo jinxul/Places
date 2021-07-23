c90ff63762654a52ed8dbcf389091635bf0af1ff
jinxul <ahmadgivekesh@gmail.com>
2021-07-23 20:01:28 +0430

    added network indicator

--- presentation/src/main/java/com/givekesh/places/presentation/main/MainActivity.kt
+++ presentation/src/main/java/com/givekesh/places/presentation/main/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.givekesh.places.presentation.main
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.givekesh.places.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -10,14 +16,63 @@ class MainActivity : AppCompatActivity() {
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var connectivityManager: ConnectivityManager
+    private lateinit var networkRequest: NetworkRequest
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setupNetworkManager()
+    }
+
+    private fun setupNetworkManager() {
+        connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        networkRequest = NetworkRequest.Builder()
+            .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+            .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+            .addTransportType(NetworkCapabilities.TRANSPORT_CELLULAR)
+            .build()
+
+        connectivityManager.registerNetworkCallback(networkRequest, networkCallBack)
+        handleNetworkIndicator(isNetworkAvailable())
+    }
+
+    private var networkCallBack: ConnectivityManager.NetworkCallback =
+        object : ConnectivityManager.NetworkCallback() {
+            override fun onAvailable(network: Network) {
+                super.onAvailable(network)
+                runOnUiThread { handleNetworkIndicator(true) }
+            }
+
+            override fun onLost(network: Network) {
+                super.onLost(network)
+                runOnUiThread { handleNetworkIndicator(false) }
+            }
+        }
+
+    private fun handleNetworkIndicator(isNetworkAvailable: Boolean) {
+        val visibility = when (isNetworkAvailable) {
+            true -> View.GONE
+            false -> View.VISIBLE
+        }
+        binding.networkIndicatorLayout.networkIndicator.visibility = visibility
+    }
+
+    private fun isNetworkAvailable(): Boolean {
+        val activeNetwork = connectivityManager.activeNetwork ?: return false
+        val network = connectivityManager.getNetworkCapabilities(activeNetwork) ?: return false
+        return when {
+            network.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+            network.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+            else -> false
+        }
     }
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+        connectivityManager.unregisterNetworkCallback(networkCallBack)
     }
 }
